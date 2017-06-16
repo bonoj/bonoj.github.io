9a68610bcce9ce1da91108b1ba85526ad1bda725
bonoj <johnlbono@gmail.com>
2017-06-16 05:08:41 -0500

    Guess we're using js now...

--- script.js
+++ script.js
@@ -4,3 +4,19 @@
 //     });
 // });
 
+function stackDown(){
+    document.getElementById("f1_container").style.zIndex = "-1";
+}
+
+function stackUp(){
+    document.getElementById("f1_container").style.zIndex = "1";
+}
+
+
+function displayProjectModal() {
+    document.getElementById("jsTest").style.display = "block";
+}
+
+function hideProjectModal() {
+    document.getElementById("jsTest").style.display = "none";
+}
