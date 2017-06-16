46e61ec28db6d9fe0a703ffbf52971b9bec30702
bonoj <johnlbono@gmail.com>
2017-06-16 06:14:17 -0500

    Added homemade modal!

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
-// $(function() {
-//     $('.toggler').click(function() {
-//         $(this).find('div').slideToggle();
-//     });
-// });
-
-function stackDown(){
-    document.getElementById("f1_container").style.zIndex = "-1";
+function displayProjectModal(projectId) {
+    switch(projectId) {
+    case 1:
+        document.getElementById("proj1").style.display = "block";
+        break;
+    case 2:
+        document.getElementById("proj2").style.display = "block";
+        break;
+    case 3:
+        document.getElementById("proj3").style.display = "block";
+        break;
+    case 4:
+        document.getElementById("proj4").style.display = "block";
+        break;
+    case 5:
+        document.getElementById("proj5").style.display = "block";
+        break;
+    case 6:
+        document.getElementById("proj6").style.display = "block";
+        break;
+    }
 }
 
-function stackUp(){
-    document.getElementById("f1_container").style.zIndex = "1";
-}
-
-
-function displayProjectModal() {
-    document.getElementById("jsTest").style.display = "block";
-}
-
-function hideProjectModal() {
-    document.getElementById("jsTest").style.display = "none";
+function hideProjectModal(projectId) {
+    switch(projectId) {
+    case 1:
+        document.getElementById("proj1").style.display = "none";
+        break;
+    case 2:
+        document.getElementById("proj2").style.display = "none";
+        break;
+    case 3:
+        document.getElementById("proj3").style.display = "none";
+        break;
+    case 4:
+        document.getElementById("proj4").style.display = "none";
+        break;
+    case 5:
+        document.getElementById("proj5").style.display = "none";
+        break;
+    case 6:
+        document.getElementById("proj6").style.display = "none";
+        break;
+    }
 }
